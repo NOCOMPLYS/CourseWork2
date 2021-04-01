f7ff8d860ce3f3d86759d22fc698a6963a755418
NOCOMPLYS <74566080+NOCOMPLYS@users.noreply.github.com>
2021-04-01 22:29:43 +0300

    Add files via upload

---
+++ CourseWork2/cmake-build-debug/CMakeFiles/CourseWork2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CourseWork2.dir/main.c.obj"
+  "CourseWork2.exe"
+  "CourseWork2.exe.manifest"
+  "CourseWork2.pdb"
+  "libCourseWork2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CourseWork2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CourseWork2/cmake-build-debug/CMakeFiles/CourseWork2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CourseWork2.
+# This may be replaced when dependencies are built.
